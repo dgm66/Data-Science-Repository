3d6b1a0d30509479f10751f1af7382a745c76c10
David Gonzalez <dgm66@localhost>
2015-11-14 21:14:31 +0100

    Variable number of arguments

--- planesWithEngines_v2.sh
+++ planesWithEngines_v2.sh
@@ -8,11 +8,11 @@
 
 # Result: List the aircrafts with the given number of engines
 
-if [ $# -ne 2 ] ; then
+if [ -z $1 ] ; then
     echo "usage: planesWithEngines [file_name] [n_engines] "
 else
     if [ -a $1 ] ; then
-        csvgrep -d'^' -c nb_engines -m $2 $1 | csvcut -c manufacturer,model | tail -n +2 | sort
+        csvsort -d'^' -c manufacturer,model $1 | awk -F, '$7=="'$2'" || $7=="nb_engines"' | csvcut -c manufacturer,model | tail -n +2
     else
         echo "file not found"
     fi
